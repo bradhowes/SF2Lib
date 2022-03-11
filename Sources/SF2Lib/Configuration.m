593db120453f7d8b581fc82496ddcf629057023c
Brad Howes <bradhowes@mac.com>
2022-03-11 18:11:53 +0100

    Turn off sound playing in unit tests by default

--- Sources/SF2Lib/Configuration.m
+++ Sources/SF2Lib/Configuration.m
@@ -4,7 +4,7 @@
 
 @implementation Configuration
 
-static Configuration* shared = nullptr;
+static Configuration* shared = NULL;
 static dispatch_once_t onceToken;
 
 + (id)shared:(NSDictionary *)overrides {
@@ -22,8 +22,8 @@ static dispatch_once_t onceToken;
 }
 
 + (void)reset {
-  shared = nullptr;
-  onceToken = {};
+  shared = NULL;
+  onceToken = NULL;
 }
 
 - (instancetype)init:(nullable NSDictionary*)overrides {
@@ -31,12 +31,13 @@ static dispatch_once_t onceToken;
     NSString* path = [SWIFTPM_MODULE_BUNDLE pathForResource:@"Configuration" ofType:@"plist"];
     NSMutableDictionary* config = [[NSMutableDictionary alloc] init];
     [config addEntriesFromDictionary:[NSDictionary dictionaryWithContentsOfFile:path]];
-    if (overrides != nullptr) {
+    if (overrides != NULL) {
       [config addEntriesFromDictionary:overrides];
     }
     _config = config;
     _loggingBase = _config[@"loggingBase"];
-    _testsPlayAudio = _config[@"testsPlayAudio"];
+    NSNumber* tmp = _config[@"testsPlayAudio"];
+    _testsPlayAudio = tmp != NULL && [tmp boolValue] == YES;
   }
   return self;
 }
