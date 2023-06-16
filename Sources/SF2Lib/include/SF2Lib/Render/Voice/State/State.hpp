2e5f3b9cea580ce6279a06c3ba67a1a4e788752b
Brad Howes <bradhowes@mac.com>
2023-06-16 21:28:58 +0200

    Migrate changes from failed-audio branch

--- Sources/SF2Lib/include/SF2Lib/Render/Voice/State/State.hpp
+++ Sources/SF2Lib/include/SF2Lib/Render/Voice/State/State.hpp
@@ -45,7 +45,10 @@ public:
    @param channelState the MIDI channel that is in control
    */
   State(Float sampleRate, const MIDI::ChannelState& channelState) noexcept :
-  sampleRate_{sampleRate}, channelState_{channelState} {}
+  sampleRate_{sampleRate}, channelState_{channelState}, eventKey_{}, eventVelocity_{}
+  {
+    setDefaults();
+  }
 
   /** Create new state vector for testing purposes.
    @param sampleRate the sample rate of audio being rendered
